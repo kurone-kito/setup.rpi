99c2253233a8a428ce3be4fd321daa883a1a0548
kurone-kito <krone@kit.black>
2021-08-09 20:56:18 +0900

    feat: added the provisioning that additional packages

--- .setup/additional.sh
+++ .setup/additional.sh
@@ -5,4 +5,7 @@ set -eu
 
 export DEBIAN_FRONTEND=noninteractive
 
-apt-get -y install language-pack-ja ubuntu-desktop update-manager wiringpi
+apt-get -y install awscli gawk git jq fonts-ipafont language-pack-ja \
+  nodejs npm ntp nyancat openssh-server pulseaudio-module-bluetooth \
+  pulseaudio-utils pinentry-curses readline-common sed sl thefuck \
+  ubuntu-desktop unattended-upgrades update-manager wget wiringpi
